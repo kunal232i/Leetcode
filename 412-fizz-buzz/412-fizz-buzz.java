11ac431044fcc35e36e9f3caa77f5f6f82226f8a
kunal_ingawale <81668653+kunal232i@users.noreply.github.com>
2022-08-01 22:59:39 +0530

    Time: 10 ms (5.08%), Space: 43.6 MB (87.89%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+      List<String> ans = new ArrayList<>(n);
+      
+      for(int i=1; i<=n; i++){
+        boolean  divisibleBy3 = i % 3==0;
+        boolean divisibleBy5 = i% 5==0;
+        
+        String currStr = "";
+        
+        if(divisibleBy3){
+          currStr += "Fizz";
+        }
+        if(divisibleBy5){
+          currStr += "Buzz";
+        }
+        if(currStr.isEmpty()){
+          currStr += i;
+        }
+        ans.add(currStr);
+ 
+      }
+      return ans;
+        
+    }
+}
