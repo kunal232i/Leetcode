a8a0c2ecff29fa779524455529f515ccc396b74e
kunal_ingawale <81668653+kunal232i@users.noreply.github.com>
2022-08-18 11:52:51 +0530

    Time: 1 ms (42.80%), Space: 43.1 MB (9.39%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,21 +1,20 @@
 class Solution {
     public void sortColors(int[] arr) {
-        
-        boolean swapped;
-        for(int i=0; i<arr.length; i++){
-            swapped = false;
-            for(int j=1; j<arr.length-i; j++) {
-                if (arr[j] < arr[j - 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j - 1];
-                    arr[j - 1] = temp;
-                    swapped=true;
-                }
+        int zIndex=0, tIndex = arr.length-1, i=0;
+        while(i <= tIndex){
+            if( arr[i] == 0 ){
+                swap(arr, zIndex++, i++);
             }
-            if(!swapped){
-                break; 
-            }
+            else if(arr[i] == 2){
+                swap(arr, tIndex--, i);
+            }else
+                i++;
         }
-        
+    }
+    
+    public void swap(int[] nums,int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
