2c84502b1eb382628f7da3bc0d4603fcd02033a1
kunal_ingawale <81668653+kunal232i@users.noreply.github.com>
2022-10-15 10:45:04 +0530

    Time: 23 ms (97.08%), Space: 46.7 MB (89.56%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,28 +1,30 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-    int target = 0;
-    ArrayList<List<Integer>> result = new ArrayList<>();
-    Arrays.sort(nums);
-        for(int i = 0 ; i<=nums.length-1 ;i++){
-        int rem  = target - nums[i] ;
-        int front = i+1;
-        int back = nums.length-1;
-        while(front < back){
-            int twoSum = nums[front] + nums[back];
-            if(twoSum < rem)front++;
-            else if(twoSum >rem)back--;
-            else{
-                List<Integer> triplet = new ArrayList<>();
-                triplet.add(nums[i]);
-                triplet.add(nums[front]);
-                triplet.add(nums[back]);
-                result.add(triplet);
-                while(front < back && nums[front] == triplet.get(1))front++;
-                while(front < back && nums[back] == triplet.get(2))back--;
+        int len = nums.length-1;
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        for(int i=0;i<=len;i++) {
+            int front = i+1;
+            int back = len;
+            while(front<back) {
+                int threeSum = nums[i]+nums[front]+nums[back];
+
+                if (threeSum > 0 ) {
+                    back--;
+                }else if (threeSum < 0){
+                    front++;
+                }else{
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[front]);
+                    list.add(nums[back]);
+                    result.add(list);
+                    while(front<back && nums[front]==list.get(1))front++;
+                    while(front<back && nums[back]==list.get(2))back--;
+                }
             }
+            while(i + 1 <= len && nums[i]==nums[i+1])i++;
         }
-        while( i+1<=nums.length-1 && nums[i] == nums[i+1]) i++;
-    }
-        return result;
+       return result;
     }
 }
