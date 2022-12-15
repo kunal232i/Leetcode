5be56637bc918b2f04184b38bbfa06e2b9aa35e5
kunal_ingawale <81668653+kunal232i@users.noreply.github.com>
2022-12-15 19:32:52 +0530

    Time: 14 ms (98.18%), Space: 39.6 MB (99.80%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.java
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.java
@@ -3,18 +3,21 @@ class Solution {
         int n = points.length;
         
         if(n<=2) return n;
-        int max = 2;
+ 
+        int max = Integer.MIN_VALUE;
         for(int i=0;i<n;i++){
-            for(int j=i+1; j<n; j++){
-                int x1=points[i][0], y1=points[i][1], x2=points[j][0], y2=points[j][1];
-                int count=2;
-                for(int m=0; m<n; m++){
-                    int x3=points[m][0], y3=points[m][1];
-                    if(m != i && m != j){
-                        if((x2-x1) * (y3-y1) == (x3-x1) * (y2-y1))count++;
+            for(int j=i+1;j<n;j++){
+                int count = 2;
+                int x1 = points[i][0], y1 = points[i][1];
+                int x2 = points[j][0], y2 = points[j][1];
+            
+                for(int m=j+1;m<n;m++){
+                    int x3 = points[m][0], y3 = points[m][1];
+                    if(m!=i && m!=j){
+                        if(((y2-y1) * (x3-x1)) == ((y3-y1) * (x2-x1))) count++;
                     }
                 }
-                max = Math.max(max,count);
+                max = Math.max(count,max);
             }
         }
         return max;
