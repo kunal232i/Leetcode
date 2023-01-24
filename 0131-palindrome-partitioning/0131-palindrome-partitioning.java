3055bd2774184eca093973dc5a81a88f1cdc2b4b
kunal_ingawale <81668653+kunal232i@users.noreply.github.com>
2023-01-24 10:28:00 +0530

    Time: 22 ms (26.46%), Space: 136.2 MB (42.18%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,35 +1,33 @@
 class Solution {
    
-     public void rec(List<List<String>> ans, String s, List<String> ds){
-       if(s == null || s.length() == 0){
+     public void rec(List<List<String>> ans, String s, int ind, List<String> ds){
+       if(ind == s.length()){
            ans.add(new ArrayList<>(ds));
            return;
        }
        
-        for(int i=1; i<=s.length(); i++){
-            String temp = s.substring(0, i);
-            if(!isPalindrome(temp)) continue; 
-                ds.add(temp);
-                rec(ans, s.substring(i, s.length()), ds);
+        for(int i=ind; i<s.length(); i++){
+            if(isPalindrome(s, ind, i)){  
+                ds.add(s.substring(ind, i+1));
+                rec(ans, s, i+1, ds);
                 ds.remove(ds.size()-1);
-        }
-        return;
-       
+            }
+        }       
    }
     
-    public boolean isPalindrome(String s){
-        for(int i=0;i<s.length()/2;i++) {
-            if(s.charAt(i)!=s.charAt(s.length()-i-1)) {
+    public boolean isPalindrome(String s, int start, int end){
+       while(start <= end){
+            if(s.charAt(start++) != s.charAt(end--)) {
                 return false;
             }
-        }   
+        }
         return true;
     }
 
     
     public List<List<String>> partition(String s) {
         List<List<String>> ans = new ArrayList<>();
-        rec(ans, s, new ArrayList<>());
+        rec(ans, s, 0, new ArrayList<>());
         return ans;
     }
 }
