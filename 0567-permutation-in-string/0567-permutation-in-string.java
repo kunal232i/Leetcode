f90547b6c5e72e94c98be89f80e70d10dd556a62
kunal_ingawale <81668653+kunal232i@users.noreply.github.com>
2023-02-04 14:02:06 +0530

    Time: 1083 ms (9.34%), Space: 42.5 MB (49.87%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,18 +1,28 @@
-class Solution {
-
-    public String sort(String s) {
-        char[] t = s.toCharArray();
-        Arrays.sort(t);
-        return new String(t);
-    }
+public class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        
-        s1 = sort(s1);
+        if (s1.length() > s2.length())
+            return false;
+        HashMap<Character, Integer> map1 = new HashMap<>();
+
+        for (int i = 0; i < s1.length(); i++)
+            map1.put(s1.charAt(i), map1.getOrDefault(s1.charAt(i), 0) + 1);
+
         for (int i = 0; i <= s2.length() - s1.length(); i++) {
-            // System.out.println(sort(s2.substring(i, i + s1.length())));
-            if (s1.equals(sort(s2.substring(i, i + s1.length()))))
+            HashMap<Character, Integer> map2 = new HashMap<>();
+            for (int j = 0; j < s1.length(); j++) {
+                map2.put(s2.charAt(i + j), map2.getOrDefault(s2.charAt(i + j), 0) + 1);
+            }
+            if (matches(map1, map2))
                 return true;
         }
         return false;
+    }
+
+    public boolean matches(HashMap<Character, Integer> map1, HashMap<Character, Integer> map2) {
+        for (char key : map1.keySet()) {
+            if (map1.get(key) - map2.getOrDefault(key, -1) != 0)
+                return false;
+        }
+        return true;
     }
 }
