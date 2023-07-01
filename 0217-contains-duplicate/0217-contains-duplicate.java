7e1d0ecf638a39d7020deeeb8788a723f990614b
kunal_ingawale <81668653+kunal232i@users.noreply.github.com>
2023-07-01 20:18:30 +0530

    Time: 20 ms (37.01%), Space: 57.3 MB (27.06%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,18 +1,9 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        
-        HashMap<Integer, Integer> tb = new HashMap<>();
-
-        
-        for(int num : nums){
-            tb.put(num, tb.getOrDefault(num,0)+1);
-            
-            if(tb.get(num) != 1){
-                return true;
-            }
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0; i<nums.length; i++){
+            set.add(nums[i]);
         }
-        return false;
-        
-        
+        return set.size() != nums.length ? true : false;
     }
 }
