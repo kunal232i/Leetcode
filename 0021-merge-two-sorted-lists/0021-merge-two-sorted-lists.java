23e3fab7b9124624c7bb52a784400fc20c6e42d8
kunal_ingawale <81668653+kunal232i@users.noreply.github.com>
2023-07-09 21:59:54 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (50.03%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -14,16 +14,19 @@ class Solution {
         ListNode curr = prev;
         
         while(list1 != null && list2 != null){
-            if(list1.val < list2.val){
+            if(list1.val <= list2.val){
                 curr.next = list1;
                 list1 = list1.next;
             }else{
                 curr.next = list2;
-                list2 = list2.next;          
+                list2 = list2.next;
             }
             curr = curr.next;
         }
+
         curr.next = list1 == null ? list2 : list1;
+        
         return prev.next;
+        
     }
 }
