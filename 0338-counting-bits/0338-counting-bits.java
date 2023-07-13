d4761eedd290bce4e0e8a8b0933d5f9a2ecc2989
kunal_ingawale <81668653+kunal232i@users.noreply.github.com>
2023-07-13 16:55:28 +0530

    Time: 2 ms (59.97%), Space: 46.3 MB (92.05%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,18 +1,18 @@
 class Solution {
     
-    public int countSetBits(int num){
-        int cnt = 0;
-        while(num>0){
-            cnt += num&1;
-            num >>= 1;
-        }
-        return cnt;
-    }
+    // public int countSetBits(int num){
+    //     int cnt = 0;
+    //     while(num>0){
+    //         cnt += num&1;
+    //         num >>= 1;
+    //     }
+    //     return cnt;
+    // }
     
     public int[] countBits(int n) {
         int arr[] = new int[n+1];
         for(int i=0; i<arr.length; i++){
-            arr[i] = countSetBits(i);
+            arr[i] = Integer.bitCount(i);
         }
         return arr;
     }
