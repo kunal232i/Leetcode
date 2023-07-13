795f50c18f6a8f1cc5314c95456a635b24fb6ce8
kunal_ingawale <81668653+kunal232i@users.noreply.github.com>
2023-07-13 17:20:12 +0530

    Time: 1 ms (96.28%), Space: 40.1 MB (99.95%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,9 @@
+public class Solution {
+    
+    
+    public int reverseBits(int n) {
+        String binary = Integer.toBinaryString(n);
+        int ans = Integer.reverse(n);
+        return ans;
+    }
+}
