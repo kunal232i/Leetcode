f1ebf9e2cbe566b2d4b05042acf0e6e1f254c980
kunal_ingawale <81668653+kunal232i@users.noreply.github.com>
2023-07-13 16:53:46 +0530

    Time: 3 ms (36.84%), Space: 46.5 MB (58.57%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -12,9 +12,7 @@ class Solution {
     public int[] countBits(int n) {
         int arr[] = new int[n+1];
         for(int i=0; i<arr.length; i++){
-            String binaryString = Integer.toBinaryString(i);
-            int binToInt = Integer.parseInt(binaryString, 2);
-            arr[i] = countSetBits(binToInt);
+            arr[i] = countSetBits(i);
         }
         return arr;
     }
