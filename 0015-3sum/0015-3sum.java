3fa1efa02758ef71aa2db28051c02ca4233fc8aa
kunal_ingawale <81668653+kunal232i@users.noreply.github.com>
2023-07-28 08:02:54 +0530

    Time: 39 ms (81.48%), Space: 51.6 MB (28.73%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,30 +1,36 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        int len = nums.length-1;
+        int n = nums.length-1;
         Arrays.sort(nums);
-        List<List<Integer>> result = new ArrayList<>();
-        for(int i=0;i<=len;i++) {
+        List<List<Integer>> res = new ArrayList<>();
+        
+        for(int i=0; i<=n; i++){
             int front = i+1;
-            int back = len;
-            while(front<back) {
-                int threeSum = nums[i]+nums[front]+nums[back];
-
-                if (threeSum > 0 ) {
-                    back--;
-                }else if (threeSum < 0){
+            int back = n;
+            
+            while(front<back){
+                int sum = nums[i] + nums[front] + nums[back];
+                List<Integer> list = new ArrayList<>();
+                
+                if(sum > 0){
+                    back--;   
+                }else if(sum < 0){
                     front++;
                 }else{
-                    List<Integer> list = new ArrayList<>();
                     list.add(nums[i]);
                     list.add(nums[front]);
                     list.add(nums[back]);
-                    result.add(list);
-                    while(front<back && nums[front]==list.get(1))front++;
-                    while(front<back && nums[back]==list.get(2))back--;
+                    res.add(list); 
+                    
+                    //check duplicate and then increse front & back
+                    while(front<back && nums[front] == list.get(1))front++;
+                    while(front<back && nums[back] == list.get(2))back--;
                 }
             }
-            while(i + 1 <= len && nums[i]==nums[i+1])i++;
+            
+            //check duplicate & then increase i
+            while(i+1<=n && nums[i]==nums[i+1])i++;
         }
-       return result;
+        return res;
     }
 }
