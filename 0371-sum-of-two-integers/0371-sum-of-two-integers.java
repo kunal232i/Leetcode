29a9c1e71c243b356ca38130a84666c1485e0029
kunal_ingawale <81668653+kunal232i@users.noreply.github.com>
2023-07-28 14:33:44 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (15.46%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.java
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.java
@@ -1,6 +1,10 @@
 class Solution {
     public int getSum(int a, int b) {
-        int sum = a + b;
-        return sum;
+        while(b!=0){
+            int tmp = (a&b)<<1; //old a val
+            a = a ^ b;
+            b = tmp;
+        }
+        return a;
     }
 }
