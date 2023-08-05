283d108c23c6ed5b56faffee5326e00e83076bdc
kunal_ingawale <81668653+kunal232i@users.noreply.github.com>
2023-08-05 08:25:20 +0530

    Time: 16 ms (11.24%), Space: 44.4 MB (10.31%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-       
         Set<String> seen = new HashSet();
         
         for(int r=0; r<9; r++){
@@ -14,55 +13,7 @@ class Solution {
                 }
             }
         }
+        System.out.print(seen);
         return true;
-        
-        /*
-        rules
-
-        hashSet -> 
-        Traverse through every row and check for duplicate
-        same for col
-
-        inserting - O(1) checking - O(1)
-
-        o(9^2)
-
-        row
-        0-3 1-3 2-3
-
-        any position like [4,4] in sudoku borad
-
-        by row form it'll be 4/3,4/3 -> [1,1]
-
-        why divide by 3 because main it is 3,3 grid
-        hashmap
-        key(r/3,c/3)
-
-
-        Sudoku
-        012345678
-        1
-        2
-        3
-        4   *
-        5
-        6
-        7
-        8
-
-
-        hashset<string> seen
-        loop through all row and col
-        if(not empty){
-            if(
-            !row in seen
-            !same for col
-            !also add subbox number)return false
-        }
-
-
-        hash set trick.
-
-        */
     }
 }
